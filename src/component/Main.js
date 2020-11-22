106317c755f0551ced291ed3424911f907e96aea
Kevin Annen <lekiyminin@gmail.com>
2020-11-22 16:24:16 -0600

    Created modal to import creature data by json

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import EncounterTracker from './encounter_tracker/EncounterTracker';
 import Navbar from './Navbar';
+import Modal from './encounter_tracker/Modal';
+import api from '../api/api';
 
 class Main extends Component {
     constructor(props){
@@ -8,7 +10,8 @@ class Main extends Component {
 
         this.state = {
             isLoading: false,
-            components: []
+            components: [],
+            useJson: false
         }
     }
 
@@ -21,10 +24,34 @@ class Main extends Component {
         this.setState({components: this.state.components.filter((component) => component.key !== key)});
     }
 
+    toggleUseJson = () => [
+        this.setState({useJson: !this.state.useJson})
+    ]
+
+    saveCreature = async (e) => {
+        e.preventDefault();
+        const data = JSON.parse(e.target[1].value);
+        api.insertMonster(data).then(() => window.alert('Creature Added '+data.name));
+        //this.toggleUseJson();
+    }
+
     render(){
         return (
             <div className="main">
-                <Navbar content={<button onClick={this.createEncounter}>Create New Encounter</button>}/>
+                <Navbar content={ <React.Fragment>
+                    <button onClick={this.createEncounter}>Create New Encounter</button> 
+                    <Modal modalName="Add New Creature" content={<React.Fragment>
+                        <form onSubmit={this.saveCreature}>
+                            <div>
+                                <label>Import From JSON: </label>
+                                <input id="use-json" type="checkbox" onClick={this.toggleUseJson}></input>
+                            </div>
+                            {this.state.useJson ? (<textarea rows="10" cols='50' />) : (<React.Fragment />)}
+                            <input type="submit" name='saveCreature' value="Save Creature"></input>
+                        </form>
+                    </React.Fragment>} />
+                </React.Fragment>}/>
+                
                 {this.state.components}
             </div>
         )
--- src/component/encounter_tracker/Modal.js
+++ src/component/encounter_tracker/Modal.js
@@ -1,20 +1,33 @@
 import React, {Component} from 'react';
 
 class Modal extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            isActive: false
+        }
+    }
+
+    toggleModal = () => {
+        this.setState({isActive: !this.state.isActive});
+    }
     
     render(){
-        if(this.props.isActive){
-            return (
+        return (<React.Fragment>
+            <button className={this.props.buttonClass+' open-modal-btn'} onClick={this.toggleModal}>{this.props.modalName}</button>
+            {this.state.isActive ?
+            (
                 <div className="modal">
-                    <button className="close-modal-btn" onClick={this.props.toggleModal}>X</button>
+                    <button className="close-modal-btn" onClick={this.toggleModal}>X</button>
                     <div className="modal-content">
                         {this.props.content}
                     </div>
                     
                 </div>
-            )
-        }
-        return (<div></div>);
+            ) : (<React.Fragment />)}
+            
+            </React.Fragment>)
     }
 }
 
